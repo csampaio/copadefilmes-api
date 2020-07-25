53ad408f7e535ed35ac1349cc8cd08ab761f75b1
Claudio Sampaio <claudio.sampaio@pmm.am.gov.br>
2020-07-24 20:58:49 -0400

    Adicionado Contexto de Filme

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using copadefilmes_api.Models;
 
 namespace copadefilmes_api
 {
@@ -25,6 +20,8 @@ namespace copadefilmes_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<FilmeContext>(opt =>
+               opt.UseInMemoryDatabase("FilmeList"));
             services.AddControllers();
         }
 
